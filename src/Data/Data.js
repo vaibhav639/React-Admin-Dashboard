131ddeb1059e5a5ac7cd908ae805bb139bd130ad
vaibhav639 <chauhanvaibhav639@gmail.com>
2023-03-15 07:12:54 -0700

    first commit

---
+++ src/Data/Data.js
@@ -0,0 +1,40 @@
+import {BiHomeAlt} from "react-icons/bi";
+import {TbClipboardList} from "react-icons/tb";
+import {RxPerson} from "react-icons/rx";
+import {HiOutlineCalendar} from "react-icons/hi";
+import {SiSimpleanalytics} from "react-icons/si";
+
+
+//Sidebar Data
+
+
+
+ const SidebarData = [
+    {
+        icon: BiHomeAlt,
+        heading: "Dashboard",
+    },
+
+    {
+        icon: TbClipboardList,
+        heading: "Orders",
+    },
+
+    {
+        icon: RxPerson,
+        heading: "Customers",
+    },
+    
+    {
+        icon: HiOutlineCalendar,
+        heading: "Products",
+    },
+
+    {
+        icon: SiSimpleanalytics,
+        heading: "Analytics",
+    },
+];
+
+export default SidebarData;
+ 
---
+++ src/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+import "./Sidebar.css";
+import Logo from "../../imgs/logo.png";
+import SidebarData from "../../Data/Data";
+import {UilSignOutAlt} from '@iconscout/react-unicons'
+
+const Sidebar = () => {
+
+const [Selected, setSelected] = useState(0)
+
+
+  return (
+    <div className="Sidebar">
+      {/* logo */}
+      <div className="logo">
+        <img src={Logo} alt="" />
+        <span>
+          Sh<span>o</span>ps
+        </span>
+      </div>
+
+      {/* Menu */}
+      <div className="menu">
+        {SidebarData.map((item, index) => {
+          return(
+            <div className= {Selected===index?'menuItem active':'menuItem'}
+            key={index}
+            onClick={()=> setSelected(index)}
+            >
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
+        })}
+        <div className="menuItem">  
+          <UilSignOutAlt/>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
